683b6e0168787c3155f195f1665f4b9a0b1688a8
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-06 16:46:03 +0300

    corrected the failed checks

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """
 
-This script defines a function called index_range that takes two arguments page and page_size
+This script defines a function called index_range that takes
+two arguments page and page_size
 
 """
+
 
 def index_range(page: int, page_size: int) -> tuple:
     """
     This function takes two integer arguments and returns a tuple of size two
-    containing a start index and an end index corrresponding to the range of indexes
-    to return in a list or those particular pagination parameters
+    containing a start index and an end index corrresponding to the range of
+    indexes to return in a list or those particular pagination parameters
 
     """
 
