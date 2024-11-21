aadcea541c4346dc5de21cff1c92699d215f69b4
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-21 18:09:24 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,7 +8,7 @@ from flask_babel import Babel
 class Config:
     """ Configuration for babel """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = ["en"]
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
